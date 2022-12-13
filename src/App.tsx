49e1c1b4c60c02338febef244c186bb148d6a80c
Hasan <hasan.jafri7@gmail.com>
2022-12-13 15:25:34 -0500

    import react to fix vscode errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from "react";
+import React, { forwardRef } from "react";
 import { ThemeProvider, createTheme } from "@mui/material";
 import MaterialTable from "material-table";
 
